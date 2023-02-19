55a0f75cfeef1f3c780b5e68eb98d7f1c359e433
Alex Pinto <contactalexpinto@gmail.com>
2023-02-19 21:56:12 +0100

    last commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Card/Card";
-import { useEffect, useState } from "react";
 
 const Home = () => {
-/*
-  const [data, setData] = useState([]);
-    
-  useEffect(() => {
-    fetch("/data/logements.json")
-    .then(response => response.json())
-    .then(res => setData(res))
-    .then(data => {console.log(data)})
-    .catch(error => console.error(error));
-  
-  }, []);
 
-*/
   return (
     <div>
       <Banner />
